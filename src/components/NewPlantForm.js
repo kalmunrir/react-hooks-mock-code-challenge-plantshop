79d7b91bd576bfd52d2df96431e84fd3b299cb65
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-30 09:52:40 -0600

    Finished core deliverables

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({addPlant}) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addPlant({
+      "name": name,
+      "image": image,
+      "price": price
+    });
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={(e) => handleSubmit(e)} >
+        <input type="text" name="name" placeholder="Plant name" onChange={(e) => setName(e.target.value)}/>
+        <input type="text" name="image" placeholder="Image URL" onChange={(e) => setImage(e.target.value)} />
+        <input type="number" name="price" step="0.01" placeholder="Price" onChange={(e) => setPrice(parseFloat(e.target.value))} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantList, setPlantList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((res) => res.json())
+    .then((plants) => setPlantList(plants))
+  }, []);
+
+  function addPlant(newPlant) {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+  .then((res) => res.json())
+  .then((newPlant) => setPlantList([...plantList, newPlant]));
+  }
+
+  const filteredPlants = plantList.filter((plant) => plant.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant}/>
+      <Search setSearchTerm={setSearchTerm}/>
+      <PlantList plantList={filteredPlants}/>
     </main>
   );
 }
